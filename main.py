10c2001925547a20e01bbbf6f0139b5b5cd92472
Reema <reema@Reemas-MacBook-Pro.local>
2024-11-24 17:56:37 +0300

    Update main.py and Procfile for Heroku deployment

--- main.py
+++ main.py
@@ -33,5 +33,7 @@ add_routes(
 )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8002)) 
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
+    uvicorn.run(app)
+
+    # host="0.0.0.0", port=8002
